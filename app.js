ba2490aa23233036416a286f4f04d9505a83fd6c
Jelle Voost <j.b.voost@gmail.com>
2015-12-23 23:00:36 +0100

    Added eslint config and mocha (no tests yet)

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({ name: 'App startup' });
 
-//setup the db
+// setup the db
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('shows.db');
 
-//define routes
+// define routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,16 +23,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'jade');
 
-app.use(function(req,res,next){
-    req.db = db;
-    next();
+app.use(function (req, res, next) {
+  req.db = db;
+  next();
 });
 
 app.use('/', routes);
 app.use('/users', users);
 
-//catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -39,7 +41,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -50,7 +52,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -58,5 +60,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
+log.info('Succesfull load');
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/list', function(req, res, next) {
-  var db = req.db
-  db.serialize(function() {
-    resultArr = [];
+router.get('/list', function (req, res, next) {
+  var db = req.db;
+  db.serialize(function () {
+    var resultArr = [];
 
     /*
 
@@ -19,10 +19,9 @@ router.get('/list', function(req, res, next) {
     stmt.finalize();
     */
 
-    db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-      console.log(row.id + ': ' + row.info);
+    db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
       resultArr.push(row.info);
-    }, function(done) {
+    }, function (done) {
       res.json(resultArr);
     });
   });
